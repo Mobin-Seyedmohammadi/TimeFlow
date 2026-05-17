3817934a521b961a738a4cd296d24c670342b653
Mobin Seyedmohammadi <mobin79.sm@gmail.com>
2026-05-17 16:37:19 +0200

    Initial Commit

---
+++ TimeFlow/TimeFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFlowApp.swift
+//  TimeFlow
+//
+//  Created by Mobin on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TimeFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
